3ad4b4a84df71274c7d2a925d261c2793a4dfec2
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-10-01 16:13:51 -0400

    Calls API for my own data, passes down to CardContainer as props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import CardContainer from './components/CardContainer';
 
 class App extends React.Component {
+  state = {
+    myData: []
+  }
   
-
+componentDidMount() {
+  axios
+  //get followers array from Github API
+  .get(`https://api.github.com/users/lorenzosimpson`)
+  .then(res => {
+    console.log(res)
+    this.setState({
+      myData: res.data
+    })
+  })
+  .catch(err => console.log(err))
+}
 
   render() {
     return (
     <div className="App">
-      <p>hello world</p>
+      <CardContainer myData={this.state.myData}/>
     </div>
   );
 }
