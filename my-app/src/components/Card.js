82e77e8a9280b7be89c6e5df6739e4ccb3a8af6a
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-10-01 19:12:02 -0400

    remove compiler warnings, adds alt

--- my-app/src/components/Card.js
+++ my-app/src/components/Card.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Card = props => {
     return (
         <div className='card'>
-            <a href={`${props.html_url}`} target='_blank'>
-                <img src={props.avatarUrl}></img>
+            <a href={`${props.html_url}`} target='_blank' rel="noopener noreferrer">
+                <img src={props.avatarUrl} alt='github follower'></img>
                 <h2>{props.login.toLowerCase()}</h2>
             </a>
         </div>
